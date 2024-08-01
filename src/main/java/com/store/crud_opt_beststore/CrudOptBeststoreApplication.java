b2ce24eddf321797d6de4ff7bf5d2c536f0591fd
Jayesh Verma <jayeshverma2303@gmail.com>
2024-08-01 14:42:58 +0530

    Adding required  files.

---
+++ src/main/java/com/store/crud_opt_beststore/CrudOptBeststoreApplication.java
@@ -0,0 +1,13 @@
+package com.store.crud_opt_beststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOptBeststoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOptBeststoreApplication.class, args);
+	}
+
+}
